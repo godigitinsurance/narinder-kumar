4d821dc0011229f9ff80e80acf528d211a04ea87
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-02-22 09:39:29 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-$("#customerName").text("Arvind Pal");
-$("#policyNumber").text("PL72052220");
+$("#customerName").text("Narinder Kumar");
+$("#policyNumber").text("PL72052296");
 $("#startDate").text("23-Feb-2020");
 $("#endDate").text("22-Feb-2021");
 
 
 $("#type").text("Two Wheeler");
-$("#registrationNumber").text("PB08CY7655");
-$("#make").text("HERO MOTOCORP");
-$("#model").text("SPLENDER PRO");
-$("#vehicalSubType").text("SELF");
-$("#engineNo").text("HA10EREHM58169");
-$("#chassis").text("MBLHA10BFEHM14234");
+$("#registrationNumber").text("PB09G6273");
+$("#make").text("HERO HONDA");
+$("#model").text("PASSION PLUS");
+$("#vehicalSubType").text("KICK START");
+$("#engineNo").text("06B08M12309");
+$("#chassis").text("06B09C22685");
